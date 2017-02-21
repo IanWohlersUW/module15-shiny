7b2edc88f3f80013891892cbc2bfda58576307d3
Ian Wohlers <ianw9@uw.edu>
2017-02-21 15:16:20 -0800

    Start exercise 2

--- exercise-2/app.R
+++ exercise-2/app.R
@@ -1,42 +1,49 @@
 # Load the shiny, ggplot2, and dplyr libraries
-
+library(shiny)
+library(ggplot2)
+library(dplyr)
 
 # You will once again be working with the `diamonds` data set provided by ggplot2
 # Use dplyr's `sample_n()` function to get a random 3000 rows from the data set
 # Store this sample in a variable `diamonds.sample`
-
+diamonds.sample <- sample_n(diamonds, 3000)
 
 # For convenience store the `range()` of values for the `price` and `carat` values
 # for the ENTIRE diamonds dataset.
-
-
+diamonds.price.range <- range(diamonds$price)
+diamonds.carat.range <- range(diamonds$carat)
 
 # Define a UI using a fluidPage layout
-
+my.ui <- fluidPage(
 
   # Include a `titlePanel` with the title "Diamond Viewer"
-
+  titlePanel("Diamond Viewer"),
 
   # Include a `sidebarLayout()`
-
+  sidebarLayout(
 
     # The `siderbarPanel()` should have the following control widgets:
-
+    sidebarPanel(
 
       # A sliderInput labeled "Price (in dollars)". This slider should let the user pick a range
       # between the minimum and maximum price of the entire diamond data set
-
+      
+      sliderInput("price.key", "Price (in dollars)", diamonds.price.range[1], diamonds.price.range[2]),
 
       # A sliderInput labeled "Carats". This slider should let the user pick a range
       # between the minimum and maximum carats of the entire diamond data set
+      
+      sliderInput("carats.key", "Carats", diamonds.carat.range[1], diamonds.carat.range[2])
 
 
       # A checkboxInput labeled "Show Trendline". It's default value should be TRUE
 
 
       # A slectInput labeled "Facet By", with choices "cut", "clarity" and "color"
+      
+    )
 
-
+  )
 
     # The `mainPanel()` should have the following reactive outputs:
 
@@ -46,10 +53,10 @@
 
       # Bonus: a dataTableOutput showing a data table of relevant observations
 
-
+)
 
 # Define a Server function for the app
-
+my.server <- function(input, output){
 
   # Assign a reactive `renderPlot()` function to the outputted `plot`
 
@@ -75,10 +82,10 @@
 
   # Bonus: Assign a reactive `renderDataTable()` function to the outputted table
   # You may want to use a `reactive()` variable to avoid needing to filter the data twice!
-
+}
 
 # Create a new `shinyApp()` using the above ui and server
-
+shinyApp(ui = my.ui, server = my.server)
 
 ## Double Bonus: For fun, can you make a similar browser for the `mpg` data set?
 ## it makes the bonus data table a lot more useful
